e9cd7d31f97438b586ba21d9775ad6fe5a955a45
aristrisnawan <aristrisnawan35@gmail.com>
2022-11-13 18:50:54 +0700

    project until login page

---
+++ lib/custom/custom_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static Color btnColor = Color(0xFF705ee8);
+  static Color blackText = Color(0xFF3d3d3f);
+  static Color grayPlaceholder = Color(0xFFC7C9D9);
+  static Color blueText = Color(0xFFbcb1fe);
+}
